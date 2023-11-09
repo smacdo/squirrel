0a1a4fcef9cb1eda8e43310096fcbed290327c70
Scott MacDonald <scott@smacdo.com>
2023-11-08 19:43:27 -0800

    configure tracing log library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use tracing_log::env_logger;
+use tracing_subscriber::{filter, prelude::__tracing_subscriber_SubscriberExt, EnvFilter};
 
 fn main() {
-    // Initialize logging.
-    env_logger::init();
+    tracing_log::LogTracer::init().expect("failed to initialize LogTracer");
+
+    let stdout_subscriber = tracing_subscriber::fmt().pretty().finish();
+    tracing::subscriber::set_global_default(stdout_subscriber)
+        .expect("failed to install stdout global tracing subscriber");
+
+    // TODO: Configure tracing to emit INFO+ for wgpu, and DEBUG+ for squirrel
+
     squirrel::squirrel_main();
 }
